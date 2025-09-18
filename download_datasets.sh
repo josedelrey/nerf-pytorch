13f00d446aa82ba99aa78285ceed593886de5184
josedelrey <josedelreyortega@gmail.com>
2025-09-18 13:53:10 +0200

    Update README.md

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,5 +1,5 @@
-mkdir -p datasets
-cd datasets
+mkdir -p dataset
+cd dataset
 wget http://cseweb.ucsd.edu/~viscomp/projects/LF/papers/ECCV20/nerf/nerf_example_data.zip
 unzip nerf_example_data.zip
 rm nerf_example_data.zip
